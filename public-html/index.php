e23df2fc60f02ae64cd23a74d211bbde9770a1a4
Youssef Hany <youssefhany9711@gmail.com>
2022-01-07 14:26:36 +0200

    Bootstrap starter template

--- public-html/index.php
+++ public-html/index.php
@@ -1,125 +1,21 @@
-<?php
-$website='https://zavrel.net/';
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>TALKER</title>
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>TALKER</h1>
 
-<!DOCTYPE html>  
-<head>  
- <title>Hello World!</title>
-</head>
-
-<body>  
- <h1>Hello World!</h1>
- <a href="<?php echo $website;?>"><?php echo "Zavrel Consulting: $website";?></a>
- <p>
-     <?php 
-     $name=array('paul','john');
-     $name[]='tom';
-     
-     print_r($name);
-     
-     ?>
- </p>
- <p><?php echo $name[1] . count($name) . strlen($name[1]);?></p>
- <p>
-    <?php 
-    $age=[
-        "john"=>11,"paul"=>23,"agmed"=>51
-    ];
-    print_r($age);
-    echo "<br>";
-    echo $age["john"];
-    ?>
-</p>
-<script>
-
-        var cars=["Mercedes","Volvo","BMW","Tesla"];
-        for (i in cars){
-            console.log("The current car is "+cars[i]);
-        }
-</script>
-<?php
-    $cars=['Mercedes',"Volvo","BMW","Tesla"];
-    foreach ($cars as $i){
-        echo "The current car is $i<br>";
-    }
-?>
-<p>
-    <?php
-    Class carBluePrint{
-        //properties of class
-        //public $colour="blue";
-        //public $make="BMW";
-
-        //Constructor
-        public function __construct($newColor,$newMake){
-            $this->colour=$newColor;
-            $this->make=$newMake;
-        }
-
-        //methods of class
-        //setter method for colour
-        public function setColor($color){
-            $this->colour=$color;
-        }
-        //getter method for colour
-        public function getColor(){
-            return "<br>New Color is: $this->colour<br>";
-        }
-    }
-
-    $firstRealCar=new carBluePrint("GREEN","VOLVO");
-    echo $firstRealCar->getColor() . $firstRealCar->make . "<br>";
-
-    var_dump($firstRealCar);
-    echo $firstRealCar->colour;
-
-    $firstRealCar->setColor("BROWN");
-    echo $firstRealCar->getColor();
-
-    $secondRealCar=new carBluePrint("BLACK","VOLVO");
-    echo $secondRealCar->getColor() . $secondRealCar->make . "<br>";
-
-    $secondRealCar->setColor("PURPLE");
-    echo $secondRealCar->getColor();
-    var_dump($secondRealCar);
-    ?>
-</p>
-
-<p>
-    <?php
-    class sportCarBluePrint extends carBluePrint{
-        
-        public function __construct($newColor,$newMake,$newSpoiler){
-            parent::__construct($newColor,$newMake);
-            $this->spoiler=$newSpoiler;
-        }
-
-        public function activateSpoiler(){
-            return "<br><strong>SPOILER ACTIVE!</strong><br>";
-        }
-    }
-
-    $firstSportCar=new sportCarBluePrint('magenta','Porsche','tail');
-    $firstSportCar->setColor("PINK");
-    var_dump($firstSportCar);
-    echo $firstSportCar->activateSpoiler();
-
-    ?>
-</p>
-<p>
-    <?php
-    function divideOnebyNumber($number){
-        if($number==0){
-            throw new Exception("Division by Zero is not allowed.");
-        }
-        return 1/$number;
-    }
-    try{
-        echo "Result of Division: " . divideOnebyNumber(0);
-    }
-    catch(Exception $e){
-        echo 'Message: ' . $e->getMessage();
-    }
-    ?>
-</p>
-</body>
+    <!-- Optional Javascript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
